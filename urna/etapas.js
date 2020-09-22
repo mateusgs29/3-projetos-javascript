a09987ae1d385f93092af1ec270cc7d81530d60c
Mateus Gomes <mateus.gs1999@gmail.com>
2020-09-22 14:59:41 -0300

    projeto da urna eletronica

---
+++ urna/etapas.js
@@ -0,0 +1,66 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '55123',
+                nome: 'José',
+                partido: 'ABC',
+                fotos: [
+                    {url: '55123.jpg', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '12345',
+                nome: 'Antonella',
+                partido: 'DEFG',
+                fotos: [
+                    {url: '12345.jpg', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '87654',
+                nome: 'Rita',
+                partido: 'ABC',
+                fotos: [
+                    {url: '87654.jpg', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '92001',
+                nome: 'Fábio',
+                partido: 'DEFG',
+                fotos: [
+                    {url: '92001.jpg', legenda: 'Vereador'}
+                ]
+            }
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '68',
+                nome: 'Lívia',
+                partido: 'ABC',
+                vice: 'Sebastião',
+                fotos: [
+                    {url: '68.jpg', legenda: 'Prefeito'},
+                    {url: '68_2.jpg', legenda: 'Vice-Prefeito', small: true}
+                ] 
+            },
+            {
+                numero: '42',
+                nome: 'Manuel',
+                partido: 'DEFG',
+                vice: 'Martin',
+                fotos: [
+                    {url: '42.jpg', legenda: 'Prefeito'},
+                    {url: '42_2.jpg', legenda: 'Vice-Prefeito', small: true}
+                ] 
+            }
+        ]
+    }
+]
