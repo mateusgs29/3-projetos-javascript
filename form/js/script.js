86ba161df2695e2ffb0826b2cffdf970e8c8f65c
Mateus Gomes <mateus.gs1999@gmail.com>
2020-09-24 14:40:41 -0300

    base do script finalizada

--- form/js/script.js
+++ form/js/script.js
@@ -1,17 +1,18 @@
 let Validator = {
     handleSubmit: (event) =>{
         event.preventDefault();
-        let send = false;
+        let send = true;
         
         let inputs = form.querySelectorAll('input');
+
+        Validator.clearErrors();
 
         for (let i = 0; i < inputs.length; i++) {
             let input = inputs[i];
             let check = Validator.checkInput(input);
-            console.log(check);
             if(check !== true){
                 send = false;
-                console.log(check);
+                Validator.showError(input, check);
             }   
         }
 
@@ -19,7 +20,6 @@ let Validator = {
             form.submit();
         }
     },
-
     checkInput: (input) => {
         let rules = input.getAttribute('data-rules');
         if(rules !== null){
@@ -30,17 +30,47 @@ let Validator = {
                 
                 switch(rDetails[0]) {
                     case 'required':
-                            if(input.value == '') {
-                                return 'Campo não pode ser vazio.';    
-                            }
+                        if(input.value == '') {
+                            return 'Campo não pode ser vazio.';    
+                        }
                         break;
                     case 'min':
+                        if(input.value.length < rDetails[1]){
+                            return 'Campo tem que ter pelo menos ' + rDetails[1] + ' caracteres.';
+                        }
+                        break;
+                    case 'email':
+                        const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+                        if(!re.test(input.value.toLowerCase())){
+                            return 'E-mail digitado não é válido.';
+                        }
                         break;
                 }
             }
         }
         
         return true; 
+    },
+    showError: (input, error) => {
+        input.style.borderColor = '#E1474D';
+
+        let errorElement = document.createElement('div');
+        errorElement.classList.add('error');
+        errorElement.innerHTML = error;
+
+        input.parentElement.insertBefore(errorElement, input.ElementSibling);
+    },
+    clearErrors: () => {
+        let inputs = form.querySelectorAll('input');
+        for (let i = 0; i < inputs.length; i++) {
+            inputs[i].style = '';
+            
+        }
+        
+        let errorElements = document.querySelectorAll('.error');
+        for (let e = 0; e < errorElements.length; e++) {
+            errorElements[e].remove();
+        }        
     }
 };
 
