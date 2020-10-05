9cae0bd36e3b4b0596a64737d885548712d8c262
Mateus Gomes <mateus.gs1999@gmail.com>
2020-10-05 16:21:07 -0300

    funções adicionadas e atualizadas

--- slidershow/js/script.js
+++ slidershow/js/script.js
@@ -1,32 +1,48 @@
-let totalSlides = document.querySelectorAll('.slider_item').length;
-let slideAtual = 0;
+let slideAtual = 0; // slide que está sendo mostrado atualmente
+let sliderWidth = document.querySelector('.slider_width');
+let totalSlides = 0;
 
-document.querySelector('.slider_width').style.width = 
-    `calc(100vw * ${totalSlides})`;
-document.querySelector('.slider_controls').style.height = 
-    `${document.querySelector('.slider').clientHeight}px`;
+// Definir altura dos controles
+document.querySelector('.slider_controls').style.height =
+    `${document.querySelector('.box').clientHeight}px`;
 
-function goPrev(){
-    slideAtual --;
-    if (slideAtual < 0) {
-        slideAtual = totalSlides - 1;
+function inicio() {
+    // colocando
+    let itens = '';
+    for (let i = 0; i < imgSlide.length; i++) {
+        itens += `<div class="slider_item"></div>`;
     }
+    sliderWidth.innerHTML = itens;
+
+    totalSlides = document.querySelectorAll('.slider_item').length;
+    // definindo a largura baseado na quantidade de fotos
+    sliderWidth.style.width = `calc(100vw * ${totalSlides})`;
+    colocarFotos();
+}
+
+function colocarFotos() {
+    let slides = document.querySelectorAll('.slider_item');
+    for (let i = 0; i < imgSlide.length; i++) {
+        slides[i].style.backgroundImage = `url(img/${imgSlide[i].url})`;
+    }
+}
+
+function goPrev() {
+    slideAtual = slideAtual > 0 ? slideAtual - 1 : totalSlides - 1;
     updateMargin();
 }
 
-function goNext(){
-    slideAtual++;
-    if(slideAtual > (totalSlides - 1)){
-        slideAtual = 0;
-    }
+function goNext() {
+    slideAtual = slideAtual === (totalSlides - 1) ? slideAtual = 0 : slideAtual + 1;
     updateMargin();
 }
 
-function updateMargin(){
-    let sliderWidth = document.querySelector('.slider_item').clientWidth;
-    let newMargin = (slideAtual * sliderWidth);
-    document.querySelector('.slider_width').style.marginLeft =
+function updateMargin() {
+    let sliderItem = document.querySelector('.slider_item').clientWidth;
+    let newMargin = (slideAtual * sliderItem);
+    sliderWidth.style.marginLeft =
         `-${newMargin}px`;
 }
 
-setInterval(goNext, 2000);
+inicio();
+setInterval(goNext, 4500); // passa de slide automáticamente
